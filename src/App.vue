f4746aec8c74eba7ac2c4e802dba65710bfe80db
Ong Jiong Hui <ongg.jh@gmail.com>
2025-10-07 18:28:37 +0800

    navbar responsive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import Sidebar from './components/navbar/Sidebar.vue'
+import Sidebar from './components/navbar/navbar.vue'
 </script>
 
 <template>
